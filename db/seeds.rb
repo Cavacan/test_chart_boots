6908b24e3512bbefa8d3baddd33289cb2009a150
田中元気 <cavacan@Cavacan-MBP.local>
2025-04-20 07:48:23 +0900

    add seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,10 @@
-# This file should ensure the existence of records required to run the application in every environment (production,
-# development, test). The code here should be idempotent so that it can be executed at any point in every environment.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Example:
-#
-#   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
-#     MovieGenre.find_or_create_by!(name: genre_name)
-#   end
+User.destroy_all
+StepRecord.destroy_all
+
+user_a = User.create!(email: "a@example.com", password: "password")
+user_b = User.create!(email: "b@example.com", password: "password")
+
+(1..31).each do |day|
+  StepRecord.create!(user: user_a, date: Date.new(2025, 1, day), steps: rand(3000..10000))
+  StepRecord.create!(user: user_b, date: Date.new(2025, 1, day), steps: rand(3000..10000))
+end
