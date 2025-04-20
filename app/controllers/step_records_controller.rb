1a782df1b50ebe9a5456a876711dde7d5b629265
田中元気 <cavacan@Cavacan-MBP.local>
2025-04-20 12:13:04 +0900

    add chart interval week and month

--- app/controllers/step_records_controller.rb
+++ app/controllers/step_records_controller.rb
@@ -1,6 +1,17 @@
 class StepRecordsController < ApplicationController
   def index
+    @interval = params[:interval] || "day"
+
     @step_records = current_user.step_records.order(date: :asc)
-    @step_data = @step_records.group(:date).sum(:steps)
+
+    @chart_data = 
+      case @interval
+      when "week"
+        @step_records.group_by_week(:date).sum(:steps)
+      when "month"
+        @step_records.group_by_month(:date).sum(:steps)
+      else
+        @step_records.group_by_day(:date).sum(:steps)
+      end
   end
 end
